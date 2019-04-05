9210f8507024c044cfb9c1c778e2b9556f326a14
David Higgins <thehig@gmail.com>
2019-04-05 22:28:45 +0100

    Write JSON to file

--- src/app.js
+++ src/app.js
@@ -1,59 +1,86 @@
-var rp = require("request-promise-native");
-var cheerio = require("cheerio"); // Basically jQuery for node.js
+const request = require("request-promise-native");
+const cheerio = require("cheerio");
+const fs = require('fs-extra');
+const path = require('path');
 
-var options = {
-  uri: "http://regalgoblins.com/spells-5e.php",
-  transform: function(body) {
-    console.log("Transforming");
-    return cheerio.load(body);
-  }
-};
+const jsonfile = path.join(__dirname, 'spells.json');
 
-const verbose = false;
-
-function extractCardDetails($, cardDiv) {
+function scrapeCard($, cardDiv) {
   const $cardDiv = $(cardDiv);
-  const id = $cardDiv.attr("data-id");
-  console.log(`Extract card ${id}`);
 
-  const level = $cardDiv.find("#level").text();
-  if(verbose) console.log(`       level: ${level}`);
-
-  const components = $cardDiv.find("#components > span").map(function() {
-    return $(this).text();
-  });
-  if(verbose) console.log(`       components: ${components}`);
-
-  const name = $cardDiv.find("#basics").text();
-  if(verbose) console.log(`       name: ${name}`);
-
-  if(verbose) console.log(`       attributes:`);
-  const attributes = $cardDiv.find('dt').map(function() {
-    const key = $(this).text().trim();
-    const value = $(this).next().text().trim();
-    if(verbose) console.log(`               ${key}: ${value}`);
-    return { [key]: value };
-  });
-
-  const description = $cardDiv.find('#description-content').text().trim();
-  if(verbose) console.log(`       description: ${description}`);
-  
-  const card = {
-    name, id, level, components, attributes, description
-  }
-
-  return card;
+  // Extract each of the pieces of the data from the DOM
+  return {
+    name: $cardDiv.find("#basics").text(),
+    id: $cardDiv.attr("data-id"),
+    level: $cardDiv.find("#level").text(),
+    components: $cardDiv.find("#components > span").map(function() {
+      return $(this).text();
+    }).get(),
+    attributes: $cardDiv.find("dt").map(function() {
+      const key = $(this)
+        .text()
+        .trim();
+      const value = $(this)
+        .next()
+        .text()
+        .trim();
+      return { [key]: value };
+    }).get(),
+    description: $cardDiv
+      .find("#description-content")
+      .text()
+      .trim()
+  };
 }
 
-console.log("Scraping");
-rp(options)
+function inferMetadata(card) {
+  const desc = card.description.toLowerCase();
+
+  return {
+    meta: {
+      isSpellAttack: desc.indexOf("spell attack") > 0,
+      isSavingThrow: desc.indexOf("saving throw") > 0,
+      hasHalfDamage: desc.indexOf("or half as much") > 0,
+      
+      // isReaction: card.attributes["Casting Time"].toLowerCase().indexOf('reaction') > 0,
+      // isBonus: card.attributes["Casting Time"].toLowerCase().indexOf('bonus') > 0,
+
+      // isConcentration: card.attributes["Duration"].toLowerCase().indexOf('concentration') > 0,
+      // // isRitual - Not in dataset
+      // isUpcastable: desc.indexOf('when you cast this spell using a spell slot of ') > 0,
+
+      hasVerbalComponent: card.components.includes("V"),
+      hasSomaticComponent: card.components.includes("S"),
+      hasMaterialComponent: card.components.includes("M")
+    }
+  };
+}
+
+function scrapeDOM() {
+  console.log("Scraping");
+  return request({
+    uri: "http://regalgoblins.com/spells-5e.php",
+    transform: function(body) {
+      console.log("Transforming");
+      return cheerio.load(body);
+    }
+  });
+}
+
+scrapeDOM()
   .then(function($) {
-    const spells = $(".card").map(function() {
-      extractCardDetails($, this);
-    });
-    return [$, spells];
+    console.log('Parsing');
+    return $(".card").map(function() {
+      return scrapeCard($, this);
+    }).get();
+  })
+  .then(function(spells) {
+    console.log('Writing to file');
+    return fs.outputJson(jsonfile, spells);
+  })
+  .then(function() {
+    console.log('done');
   })
   .catch(function(err) {
-    // Crawling failed or Cheerio choked...
     console.log(err);
   });
